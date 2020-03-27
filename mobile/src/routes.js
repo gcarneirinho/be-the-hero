ab9d4006717520331cf604cf73e75944f0d960d2
Gustavo <carneirinho@gmail.com>
2020-03-27 13:20:08 -0300

    Mobile Finalizado

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -10,9 +10,9 @@ import Detail from './pages/Detail';
 export default function Routes() {
   return(
     <NavigationContainer>
-      <AppStack.Navigator screenOptions={{headerShown: false }}>
+      <AppStack.Navigator screenOptions={{headerShown: false, gestureEnabled: true }}>
         <AppStack.Screen name="Incidents" component={Incidents} />
-        <AppStack.Screen name="Detail" component={Detail} />
+        <AppStack.Screen  name="Detail" component={Detail} />
       </AppStack.Navigator>
     </NavigationContainer>
   );
